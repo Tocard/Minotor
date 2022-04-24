3db41f05df7eea94f45bfeb5a4bef8f3d2edb30a
root <contact@ether-source.fr>
2022-04-24 19:13:17 +0000

    switch 5 to 1 min for cosmos token

--- main.go
+++ main.go
@@ -37,8 +37,8 @@ func main() {
 
 		s.Every(5).Minutes().Do(engine.GetLastEthTx)
 		s.Every(5).Minutes().Do(engine.FluxNodesOverview)
-		s.Every(5).Minutes().Do(engine.GetCosmosTokens)
 
+		s.Every(1).Minutes().Do(engine.GetCosmosTokens)
 		s.Every(1).Minutes().Do(engine.GetHiveosFarm)
 		s.Every(1).Minutes().Do(engine.GetHiveosWorkers)
 		s.Every(1).Minutes().Do(engine.GetLastEthBlock)

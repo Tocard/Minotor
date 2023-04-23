0ff9d714654dc44be1245de7293c50b71054218b
Moz <contact@ether-source.fr>
2023-04-23 16:33:41 +0200

    add chia plot check bridge

---
+++ routes/chia.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"minotor/data"
+	"minotor/es"
+)
+
+func PostChiaPlotSummary(c *gin.Context) {
+	ChiaPlotCheckSummary := data.ChiaPlotCheckSummary{}
+
+	err := c.BindJSON(&ChiaPlotCheckSummary)
+	if err != nil {
+		c.String(500, "Error on PostChiaPlotSummary | BindJson: %s ", err.Error())
+	}
+	ChiaPlotCheckSummaryJson, err := json.Marshal(ChiaPlotCheckSummary)
+	if err != nil {
+		c.String(500, "Error on PostChiaPlotSummary | Marshal: %s ", err.Error())
+	}
+	es.Bulk("minotor-chia_plot_check_summary", string(ChiaPlotCheckSummaryJson))
+	c.String(201, string(ChiaPlotCheckSummaryJson))
+}
